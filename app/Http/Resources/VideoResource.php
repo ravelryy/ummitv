fefe646e4a3fc41633bbe9968899e144eeb0fab3
ravelry <ravelry.code@gmail.com>
2024-07-24 10:28:02 +0700

    add videoId on VideoResource

--- app/Http/Resources/VideoResource.php
+++ app/Http/Resources/VideoResource.php
@@ -10,11 +10,13 @@ class VideoResource extends JsonResource
     public function toArray(Request $request)
     {
         $thumbnail = $this->getFirstMediaUrl('videos');
+        $videoId = $this->extractVideoId($this->path);
         // $tags = $this->tags->pluck('name')->toArray();
 
         return [
             'id' => $this->id,
             'path' => $this->path,
+            'videoId' => $videoId,
             'title' => $this->title,
             'slug' => $this->slug,
             'uploaded_by' => new UserResource($this->uploader),
@@ -23,6 +25,14 @@ class VideoResource extends JsonResource
             'thumbnail' => $thumbnail,
             // 'tags' => $tags,
         ];
+    }
+
+    private function extractVideoId($url)
+    {
+        $pattern = '/(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=))([^#\&\?]*)(?:\S+)?/';
+        preg_match($pattern, $url, $matches);
+
+        return isset($matches[1])? $matches[1] : null;
     }
 
 }
